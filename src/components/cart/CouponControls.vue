bde96a77782002a9c9b2a8f7c9559f7376b6d92c
Mostafa Ahmed <mostafa.ah.abdelaziz@gmail.com>
2021-09-03 14:37:36 +0200

    Order summary frontend

---
+++ src/components/cart/CouponControls.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="coupon-ctrls">
+    <div class="coupon-input">
+      <input type="text" placeholder="COUPON CODE" />
+      <button class="btn">Apply</button>
+    </div>
+
+    <div class="coupon-hint">
+      Test coupon codes: <br />
+      SUMMER15 (fixed amount)<br />
+      SCHOOLBREAK (percentage)<br />
+      WINTER30 (expired code)<br />
+    </div>
+
+    <div class="success">Coupon applied</div>
+    <div class="error">Error</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CouponControls",
+};
+</script>
+
+<style></style>
--- src/components/cart/OrderSummary.vue
+++ src/components/cart/OrderSummary.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="order-summary-wrapper">
     <div class="section-title">Order</div>
+
+    <div class="order-details">
+      <CouponControls />
+
+      <div class="order-data">
+        <div class="line">
+          <h5 class="title">You save</h5>
+          <div class="data">$25</div>
+        </div>
+        <div class="line">
+          <h5 class="title">Total amount</h5>
+          <div class="data">$132</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import CouponControls from "./CouponControls.vue";
+
 export default {
   name: "OrderSummary",
+  components: { CouponControls },
 };
 </script>
 
